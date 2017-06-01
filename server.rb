962b0ea170055aef34e7cfabe1de2e42eaef7197
Don Cooper <don_cooper@hotmail.com>
2017-06-01 15:09:01 -0400

    Links working

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['https://www.nytimes.com/', 'https://www.theglobeandmail.com/', 'https://www.washingtonpost.com/', 'https://www.theguardian.com/international', 'http://www.latimes.com/']
+  erb :favourites
 end
